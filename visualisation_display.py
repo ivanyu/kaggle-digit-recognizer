7b9984118db688a3f330849f1b3631b245763aa4
Ivan Yurchenko <ivan0yurchenko@gmail.com>
2017-01-11 19:13:41 +0200

    Add visualization

---
+++ visualisation_display.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+import numpy as np
+from matplotlib import pyplot as plt
+import meta
+from meta import data_filename
+
+
+def display(images, row_n, col_n):
+    for i in range(len(images)):
+        plt.subplot(row_n, col_n, i + 1)
+        pixels = images[i, :].reshape((28, 28))
+        plt.imshow(pixels, cmap='gray')
+    plt.show()
+
+
+if __name__ == '__main__':
+    X_test = np.load(data_filename(meta.TEST_PIXELS_BIN_FILENAME))
+    display(X_test[0:100, :], 10, 10)
