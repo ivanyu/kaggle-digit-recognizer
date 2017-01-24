eedfbb9c71648cac715405ed887096f87d5a511a
Ivan Yurchenko <ivan0yurchenko@gmail.com>
2017-01-25 01:21:03 +0200

    First consistently good result with MLP

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
 # kaggle-digit-recognizer
+
+## Results
+
+```
+Multilayer perceptron #1
+classify_mlp_1.py
+
+1 hidden layer, 1000 neurons, batchnorm, relu, dropout 0.4
+1 output layer, 10 neurons, softmax
+Xavier normal initialization
+Weight regularization: L2(0.00005)
+Optimizer: Adam with defaults
+Batch size: 64
+Learning rate schedule: steps by epochs (see file)
+
+186 epochs
+Last epoch: 2s - loss: 0.0245 - acc: 0.9989
+               - val_loss: 0.0758 - val_acc: 0.9860
+Train time: ~7 minutes (GTX 1060 6 Gb)
+Test: 0.98443
+```
+
+```
+SVM, RBF kernel
+MinMaxScaler [0, 1]
+PCA = 35_whitening
+C = 1000, γ = 0.049
+CV: [0.98334325  0.98417232  0.97904512  0.98273193  0.98237256]
+Test: 0.98471
+```
+
+```
+SVM, RBF kernel
+MinMaxScaler [0, 1]
+PCA = 35_whitening
+C = 64, γ = 0.03125
+CV: [0.98132064  0.98286326  0.97761638  0.98297011  0.98189614]
+Test: 0.98357
+```
+
+```
+kNN, k=3
+MinMaxScaler [0, 1]
+PCA=35_no_whitening
+CV: [0.97572873  0.97607997  0.96928206  0.97153745  0.97451167]
+Test: 0.97371
+```
---
+++ classify_mlp_1.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Dropout, BatchNormalization
+from keras.optimizers import Adam
+from keras.callbacks import Callback, LearningRateScheduler
+from keras.regularizers import l2
+from keras.utils import np_utils
+import matplotlib.pyplot as plt
+import meta
+from meta import data_filename
+from classify_base import load_data
+from classify_base import enumerate_and_write_predictions
+
+
+# Multilayer perceptron 1
+# 186 epochs
+# Last epoch: 2s - loss: 0.0245 - acc: 0.9989
+#                - val_loss: 0.0758 - val_acc: 0.9860
+# Train time: ~7 minutes
+# Test: 0.98443
+
+
+regularization = 0.00005
+w_regularizer = l2(regularization)
+
+model = Sequential()
+model.add(Dense(1000,
+                input_shape=(meta.IMG_WIDTH * meta.IMG_HEIGHT,),
+                init='glorot_normal',
+                activation=None, W_regularizer=w_regularizer))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(Dropout(0.4))
+model.add(Dense(10,
+                init='glorot_normal',
+                activation='softmax', W_regularizer=w_regularizer))
+
+optimizer = Adam()
+model.compile(loss='categorical_crossentropy',
+              optimizer=optimizer,
+              metrics=['accuracy'])
+
+model.summary()
+
+n_epochs = 500
+batch_size = 64
+
+nb_classes = 10
+(X_train, y_train, X_test) = load_data('minmax01')
+y_train = np_utils.to_categorical(y_train, nb_classes)
+
+
+class LearningPlotCallback(Callback):
+    def __init__(self, n_epochs):
+        super(LearningPlotCallback, self).__init__()
+        self._n_epochs = n_epochs
+
+    def on_train_begin(self, logs={}):
+        plt.ion()
+        plt.axis([0, self._n_epochs, 0, 1])
+        plt.grid()
+        self._loss = []
+        self._val_loss = []
+
+    def on_epoch_end(self, epoch, logs={}):
+        self._loss.append(logs['loss'])
+        self._val_loss.append(logs['val_loss'])
+
+        plt.plot(self._loss, 'r-')
+        plt.plot(self._val_loss, 'b-')
+        plt.pause(0.0001)
+
+
+class ValAccuracyEarlyStopping(Callback):
+    def on_epoch_end(self, epoch, logs={}):
+        if logs['val_acc'] >= 0.9850:
+            self.stopped_epoch = epoch
+            self.model.stop_training = True
+        pass
+
+
+class StepsLearningRateScheduler(LearningRateScheduler):
+    def __init__(self):
+        super(StepsLearningRateScheduler, self).\
+            __init__(StepsLearningRateScheduler._schedule)
+
+    @staticmethod
+    def _schedule(epoch):
+        if epoch < 80:
+            return 0.0010
+        elif epoch < 110:
+            return 0.0009
+        elif epoch < 120:
+            return 0.0008
+        elif epoch < 130:
+            return 0.0007
+        elif epoch < 140:
+            return 0.0006
+        elif epoch < 150:
+            return 0.0005
+        elif epoch < 160:
+            return 0.0004
+        elif epoch < 170:
+            return 0.0003
+        elif epoch < 180:
+            return 0.0002
+        elif epoch < 190:
+            return 0.0001
+        return 0.00005
+
+
+learning_plot_callback = LearningPlotCallback(n_epochs)
+val_acc_early_stopping = ValAccuracyEarlyStopping()
+
+learning_rate_scheduler = StepsLearningRateScheduler()
+
+history = model.fit(X_train, y_train,
+                    validation_split=0.1,
+                    nb_epoch=n_epochs, batch_size=batch_size,
+                    callbacks=[learning_plot_callback,
+                               val_acc_early_stopping,
+                               learning_rate_scheduler],
+                    verbose=2, shuffle=True)
+
+predictions = model.predict_classes(X_test)
+predictions = predictions.reshape((predictions.shape[0], 1))
+print(predictions)
+output_file_name = data_filename(
+    'play_nn.csv'.format()
+)
+print("Writing output file {}...".format(output_file_name))
+enumerate_and_write_predictions(predictions, output_file_name)
