2ff63a6fc592a5862bae7084c2adbade09e74837
Ivan Yurchenko <ivan0yurchenko@gmail.com>
2017-01-29 11:04:15 +0200

    Cache scaler data to speed up start. Add In-memory validation data.

--- keras_base.py
+++ keras_base.py
@@ -5,7 +5,7 @@ from __future__ import print_function
 import os
 import re
 import random
-import time
+import json
 import numpy as np
 from keras.preprocessing.image import Iterator
 from sklearn.preprocessing import MinMaxScaler
@@ -27,18 +27,16 @@ class _SubsetIterator(Iterator):
             samples_n, batch_size, shuffle, seed=None)
 
     def next(self):
-        with self.lock:
-            index_array, current_index, current_batch_size =\
-                next(self.index_generator)
+        # with self.lock:
+        index_array, current_index, current_batch_size =\
+            next(self.index_generator)
 
-        # start = time.time()
         batch_x = np.zeros((current_batch_size, meta.IMG_HEIGHT * meta.IMG_WIDTH))
         for i, j in enumerate(index_array):
             fname = self._filenames[j]
             x = self._load_image(fname)
             x = self._scaler.transform(x)
             batch_x[i] = x
-        # print('Batch x prepare, s:', time.time() - start)
 
         batch_y = np.zeros((len(batch_x), self._nb_class), dtype='float32')
         for i, label in enumerate(self._classes[index_array]):
@@ -61,7 +59,12 @@ class MnistIterators(object):
         self._directory = directory
         self._batch_size = batch_size
 
+        scaler_filename = '_scaler.json'
+
         all_files = os.listdir(directory)
+        if scaler_filename in all_files:
+            all_files.remove(scaler_filename)
+
         random.shuffle(all_files)
         split = int(len(all_files) * validation_split)
         self._train_files = all_files[0:-split]
@@ -73,6 +76,9 @@ class MnistIterators(object):
 
         class_set = set()
 
+        scaler_filepath = os.path.join(directory, scaler_filename)
+        scaler_exists = os.path.exists(scaler_filepath)
+
         self._train_classes = []
         self._valid_classes = []
         for fname in self._train_files:
@@ -82,8 +88,9 @@ class MnistIterators(object):
             class_set.add(cls)
             self._train_classes.append(cls)
 
-            x = self._load_image(fname)
-            self.scaler.partial_fit(x)
+            if not scaler_exists:
+                x = self._load_image(fname)
+                self.scaler.partial_fit(x)
 
         for fname in self._valid_files:
             m = r.match(fname)
@@ -92,14 +99,36 @@ class MnistIterators(object):
             class_set.add(cls)
             self._valid_classes.append(cls)
 
-            x = self._load_image(fname)
-            self.scaler.partial_fit(x)
+            if not scaler_exists:
+                x = self._load_image(fname)
+                self.scaler.partial_fit(x)
 
         self._nb_class = len(class_set)
         assert self._nb_class == 10
 
         self.samples_per_epoch = len(self._train_files)
         self.nb_val_samples = len(self._valid_files)
+
+        if not scaler_exists:
+            scaler_params = {
+                'scale_': list(self.scaler.scale_),
+                'min_': list(self.scaler.min_),
+                'n_samples_seen_': self.scaler.n_samples_seen_,
+                'data_min_': list(self.scaler.data_min_),
+                'data_max_': list(self.scaler.data_max_),
+                'data_range_': list(self.scaler.data_range_),
+            }
+            with open(scaler_filepath, 'w') as f:
+                json.dump(scaler_params, f, indent=4)
+        else:
+            with open(scaler_filepath, 'r') as f:
+                scaler_params = json.load(f)
+                self.scaler.scale_ = np.array(scaler_params['scale_'])
+                self.scaler.min_ = np.array(scaler_params['min_'])
+                self.scaler.n_samples_seen_ = scaler_params['n_samples_seen_']
+                self.scaler.data_min_ = np.array(scaler_params['data_min_'])
+                self.scaler.data_max_ = np.array(scaler_params['data_max_'])
+                self.scaler.data_range_ = np.array(scaler_params['data_range_'])
 
     def create_train_iterator(self):
         return _SubsetIterator(self._directory, self._train_files,
@@ -108,6 +137,11 @@ class MnistIterators(object):
                                scaler=self.scaler)
 
     def create_validation_iterator(self):
+        result = self.get_validation_data()
+        while True:
+            yield result
+
+    def get_validation_data(self):
         xs = []
         ys = []
         for fname, label in zip(self._valid_files, self._valid_classes):
@@ -118,9 +152,7 @@ class MnistIterators(object):
             y[label] = 1.
             ys.append(y)
             # y = np.expand_dims(y, axis=0)
-        result = (np.vstack(xs), np.vstack(ys))
-        while True:
-            yield result
+        return (np.vstack(xs), np.vstack(ys))
 
     def _load_image(self, fname):
         with open(os.path.join(self._directory, fname), 'rb') as fh:
